bbe2bf51ddb1b31659d9039e8a481b97f9026526
kristermyr <krister90@gmail.com>
2023-01-14 12:14:32 -0600

    added fetch request to fetch notes from api location

--- Develop/server.js
+++ Develop/server.js
@@ -26,9 +26,22 @@ res.sendFile('notes.html');
 
 // Post route for when notes gets created in HTML
 
-app.post('/note/location/temp', (req, res) => {
+app.post('/api/notes', (req, res) => {
     const newNote = req.body
     writeToFile(destination, newNote)
 
     res.json(`${req.method} received`);
 });
+
+//fetch request to add a new note
+
+const addNote = (note) => {
+    fetch('/api/notes', {
+        method: 'POST',
+        headers: { 'Conmtent-Type': 'application/json'},
+        body: JSON.stringify(note),
+    })
+        .then((res) => res.json())
+        .then((notes) => console.log(notes));
+
+    };
